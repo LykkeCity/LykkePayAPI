98077b630bebd3196542c961b087806f21b584eb
tarurar <tarutin_andrew@mail.ru>
2018-03-22 22:52:37 +0300

    added basic validation to controllers refactored BaseController

---
+++ src/Lykke.Service.PayAPI/ValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Lykke.Service.PayAPI
+{
+    public static class ValidationExtensions
+    {
+        public static bool IsValidPaymentRequestId(this string src)
+        {
+            return !string.IsNullOrWhiteSpace(src) && src.IsGuid();
+        }
+    }
+}
