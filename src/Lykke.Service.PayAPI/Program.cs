43a69b70cad9948c914b95394177951bdd2f8eb0
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-05-25 02:10:29 +0300

    LPDEV-472 revert port 5000

--- src/Lykke.Service.PayAPI/Program.cs
+++ src/Lykke.Service.PayAPI/Program.cs
@@ -24,7 +24,7 @@ namespace Lykke.Service.PayAPI
             {
                 var host = new WebHostBuilder()
                     .UseKestrel()
-                    //.UseUrls("http://*:5000")
+                    .UseUrls("http://*:5000")
                     .UseContentRoot(Directory.GetCurrentDirectory())
                     .UseStartup<Startup>()
                     .UseApplicationInsights()
