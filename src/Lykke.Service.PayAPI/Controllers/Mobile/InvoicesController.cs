0c797ea318e2ea14215f4dac55e826e72854ae2b
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-06-08 15:09:07 +0300

    LPDEV-524 add filterring by settlementAssets

--- src/Lykke.Service.PayAPI/Controllers/Mobile/InvoicesController.cs
+++ src/Lykke.Service.PayAPI/Controllers/Mobile/InvoicesController.cs
@@ -39,6 +39,7 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         /// <param name="statuses">The statuses (e.g. ?statuses=one&amp;statuses=two)</param>
         /// <param name="dispute">The dispute attribute</param>
         /// <param name="billingCategories">The billing categories (e.g. ?billingCategories=one&amp;billingCategories=two)</param>
+        /// <param name="settlementAssets">The settlement assets (e.g. ?settlementAssets=one&amp;settlementAssets=two)</param>
         /// <param name="greaterThan">The greater than number for filtering (can be fractional)</param>
         /// <param name="lessThan">The less than number for filtering (can be fractional)</param>
         /// <response code="200">A collection of invoices.</response>
@@ -50,7 +51,7 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         [ProducesResponseType(typeof(IReadOnlyList<InvoiceResponseModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetMineByFilter(IEnumerable<string> clientMerchantIds, IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, decimal? greaterThan, decimal? lessThan)
+        public async Task<IActionResult> GetMineByFilter(IEnumerable<string> clientMerchantIds, IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, IEnumerable<string> settlementAssets, decimal? greaterThan, decimal? lessThan)
         {
             var merchantId = this.GetUserMerchantId();
 
@@ -67,9 +68,9 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
                     }
                 }
 
-                var response = await _payInvoiceClient.GetByFilter(new string[] { merchantId }, clientMerchantIds, statuses, dispute, billingCategories, greaterThan, lessThan);
+                var invoices = await _payInvoiceClient.GetByFilter(new string[] { merchantId }, clientMerchantIds, statuses, dispute, billingCategories, greaterThan, lessThan);
 
-                return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(response));
+                return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(FilterBySettlementAssets(invoices, settlementAssets)));
             }
             catch (ErrorResponseException ex) when (ex.StatusCode == HttpStatusCode.BadRequest)
             {
@@ -89,6 +90,7 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         /// <param name="statuses">The statuses (e.g. ?statuses=one&amp;statuses=two)</param>
         /// <param name="dispute">The dispute attribute</param>
         /// <param name="billingCategories">The billing categories (e.g. ?billingCategories=one&amp;billingCategories=two)</param>
+        /// <param name="settlementAssets">The settlement assets (e.g. ?settlementAssets=one&amp;settlementAssets=two)</param>
         /// <param name="greaterThan">The greater than number for filtering (can be fractional)</param>
         /// <param name="lessThan">The less than number for filtering (can be fractional)</param>
         /// <response code="200">A collection of invoices.</response>
@@ -100,21 +102,21 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         [ProducesResponseType(typeof(IReadOnlyList<InvoiceResponseModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetInboxByFilter(IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, decimal? greaterThan, decimal? lessThan)
+        public async Task<IActionResult> GetInboxByFilter(IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, IEnumerable<string> settlementAssets, decimal? greaterThan, decimal? lessThan)
         {
             var merchantId = this.GetUserMerchantId();
 
             try
             {
-                var response = await _payInvoiceClient.GetByFilter(Enumerable.Empty<string>(), new string[] { merchantId }, statuses, dispute, billingCategories, greaterThan, lessThan);
+                var invoices = await _payInvoiceClient.GetByFilter(Enumerable.Empty<string>(), new string[] { merchantId }, statuses, dispute, billingCategories, greaterThan, lessThan);
 
                 //TODO: get group merchants
                 IEnumerable<string> groupMerchants = new List<string> { "Begun1" };
 
                 // should be only from merchants inside group
-                response = response.Where(x => groupMerchants.Contains(x.MerchantId)).ToList();
+                invoices = invoices.Where(x => groupMerchants.Contains(x.MerchantId)).ToList();
 
-                return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(response));
+                return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(FilterBySettlementAssets(invoices, settlementAssets)));
             }
             catch (ErrorResponseException ex) when (ex.StatusCode == HttpStatusCode.BadRequest)
             {
@@ -126,6 +128,16 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
             }
 
             return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        private IReadOnlyList<InvoiceModel> FilterBySettlementAssets(IReadOnlyList<InvoiceModel> invoices, IEnumerable<string> settlementAssets)
+        {
+            if (settlementAssets == null || !settlementAssets.Any())
+                return invoices;
+
+            invoices = invoices.Where(x => settlementAssets.Contains(x.SettlementAssetId)).ToList();
+
+            return invoices;
         }
     }
 }
