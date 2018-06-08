8911f237d818f083d61b9dcd76a7b6227ec81cf9
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-06-08 14:42:49 +0300

    LPDEV-524 update api to get mine and inbox invoices

--- src/Lykke.Service.PayAPI/Controllers/Mobile/InvoicesController.cs
+++ src/Lykke.Service.PayAPI/Controllers/Mobile/InvoicesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -34,7 +35,6 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         /// <summary>
         /// Returns invoices by filter
         /// </summary>
-        /// <param name="merchantIds">The merchant ids (e.g. ?merchantIds=one&amp;merchantIds=two)</param>
         /// <param name="clientMerchantIds">The merchant ids of the clients (e.g. ?clientMerchantIds=one&amp;clientMerchantIds=two)</param>
         /// <param name="statuses">The statuses (e.g. ?statuses=one&amp;statuses=two)</param>
         /// <param name="dispute">The dispute attribute</param>
@@ -45,16 +45,29 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
         /// <response code="400">Problem occured.</response>
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [BearerHeader]
-        [HttpGet]
-        [SwaggerOperation("InvoicesGetByFilter")]
+        [HttpGet("mine")]
+        [SwaggerOperation("InvoicesGetMineByFilter")]
         [ProducesResponseType(typeof(IReadOnlyList<InvoiceResponseModel>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.InternalServerError)]
-        public async Task<IActionResult> GetByFilter(IEnumerable<string> merchantIds, IEnumerable<string> clientMerchantIds, IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, decimal? greaterThan, decimal? lessThan)
+        public async Task<IActionResult> GetMineByFilter(IEnumerable<string> clientMerchantIds, IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, decimal? greaterThan, decimal? lessThan)
         {
+            var merchantId = this.GetUserMerchantId();
+
             try
             {
-                var response = await _payInvoiceClient.GetByFilter(merchantIds, clientMerchantIds, statuses, dispute, billingCategories, greaterThan, lessThan);
+                //TODO: get group merchants
+                IEnumerable<string> groupMerchants = new List<string> { "Begun1" };
+
+                foreach (var clientMerchantId in clientMerchantIds)
+                {
+                    if (!groupMerchants.Contains(clientMerchantId))
+                    {
+                        return BadRequest($"ClientMerchantId {clientMerchantId} is not in the group");
+                    }
+                }
+
+                var response = await _payInvoiceClient.GetByFilter(new string[] { merchantId }, clientMerchantIds, statuses, dispute, billingCategories, greaterThan, lessThan);
 
                 return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(response));
             }
@@ -64,7 +77,49 @@ namespace Lykke.Service.PayAPI.Controllers.Mobile
             }
             catch (Exception ex)
             {
-                _log.WriteError(nameof(GetByFilter), new { merchantIds, clientMerchantIds, statuses, billingCategories }, ex);
+                _log.WriteError(nameof(GetMineByFilter), new { merchantId, clientMerchantIds, statuses, billingCategories }, ex);
+            }
+
+            return StatusCode((int)HttpStatusCode.InternalServerError);
+        }
+
+        /// <summary>
+        /// Returns invoices by filter
+        /// </summary>
+        /// <param name="statuses">The statuses (e.g. ?statuses=one&amp;statuses=two)</param>
+        /// <param name="dispute">The dispute attribute</param>
+        /// <param name="billingCategories">The billing categories (e.g. ?billingCategories=one&amp;billingCategories=two)</param>
+        /// <param name="greaterThan">The greater than number for filtering (can be fractional)</param>
+        /// <param name="lessThan">The less than number for filtering (can be fractional)</param>
+        /// <response code="200">A collection of invoices.</response>
+        /// <response code="400">Problem occured.</response>
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [BearerHeader]
+        [HttpGet("inbox")]
+        [SwaggerOperation("InvoicesGetInboxByFilter")]
+        [ProducesResponseType(typeof(IReadOnlyList<InvoiceResponseModel>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetInboxByFilter(IEnumerable<string> statuses, bool? dispute, IEnumerable<string> billingCategories, decimal? greaterThan, decimal? lessThan)
+        {
+            var merchantId = this.GetUserMerchantId();
+
+            try
+            {
+                //TODO: get group merchants
+                IEnumerable<string> groupMerchants = new List<string> { "Begun1" };
+
+                var response = await _payInvoiceClient.GetByFilter(Enumerable.Empty<string>(), new string[] { merchantId }, statuses, dispute, billingCategories, greaterThan, lessThan);
+
+                return Ok(Mapper.Map<IReadOnlyList<InvoiceResponseModel>>(response));
+            }
+            catch (ErrorResponseException ex) when (ex.StatusCode == HttpStatusCode.BadRequest)
+            {
+                return BadRequest(ex.Error);
+            }
+            catch (Exception ex)
+            {
+                _log.WriteError(nameof(GetInboxByFilter), new { merchantId, statuses, billingCategories }, ex);
             }
 
             return StatusCode((int)HttpStatusCode.InternalServerError);
