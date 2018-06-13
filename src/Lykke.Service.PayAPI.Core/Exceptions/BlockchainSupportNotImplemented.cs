541e639aa52c906511c82dc0bec5fa28f42fe017
tarurar <tarutin_andrew@mail.ru>
2018-06-13 11:25:43 +0300

    added merchantWallet balances API

---
+++ src/Lykke.Service.PayAPI.Core/Exceptions/BlockchainSupportNotImplemented.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lykke.Service.PayAPI.Core.Exceptions
+{
+    public class BlockchainSupportNotImplemented : Exception
+    {
+        public BlockchainSupportNotImplemented()
+        {
+        }
+
+        public BlockchainSupportNotImplemented(string message) : base(message)
+        {
+        }
+
+        public BlockchainSupportNotImplemented(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BlockchainSupportNotImplemented(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Lykke.Service.PayAPI.Core/Exceptions/MerchantNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lykke.Service.PayAPI.Core.Exceptions
+{
+    public class MerchantNotFoundException : Exception
+    {
+        public MerchantNotFoundException()
+        {
+        }
+
+        public MerchantNotFoundException(string merchantId) : base("Merchant not found")
+        {
+            MerchantId = merchantId;
+        }
+
+        public MerchantNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MerchantNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public string MerchantId { get; set; }
+    }
+}
