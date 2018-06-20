fbd6bdc682d574710636e6a405ba0ec93f97ebab
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-06-20 19:31:45 +0300

    LPDEV-522 add api for baseAsset

---
+++ src/Lykke.Service.PayAPI/Models/Merchant/BaseAssetItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.PayAPI.Models.Merchant
+{
+    public class BaseAssetItemModel
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
