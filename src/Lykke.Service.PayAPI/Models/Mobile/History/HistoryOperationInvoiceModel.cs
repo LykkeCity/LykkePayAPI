9be65304a3033e3404f2c16cf2b3e02019628984
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-06-20 12:55:46 +0300

    Update mobile history LPDEV-587

---
+++ src/Lykke.Service.PayAPI/Models/Mobile/History/HistoryOperationInvoiceModel.cs
@@ -0,0 +1,13 @@
+﻿using Lykke.Service.PayInvoice.Client.Models.Invoice;
+
+namespace Lykke.Service.PayAPI.Models.Mobile.History
+{
+    public class HistoryOperationInvoiceModel : HistoryOperationModel
+    {
+        public string InvoiceNumber { get; set; }
+
+        public string BillingCategory { get; set; }
+
+        public InvoiceStatus InvoiceStatus { get; set; }
+    }
+}
--- src/Lykke.Service.PayAPI/Models/Mobile/History/HistoryOperationModel.cs
+++ src/Lykke.Service.PayAPI/Models/Mobile/History/HistoryOperationModel.cs
@@ -6,22 +6,24 @@ namespace Lykke.Service.PayAPI.Models.Mobile.History
     {
         public string Id { get; set; }
 
-        public string Logo { get; set; }
+        public string MerchantLogoUrl { get; set; }
 
         public string Title { get; set; }
 
-        public DateTime TimeStamp { get; set; }
+        public DateTime CreatedOn { get; set; }
 
         public decimal Amount { get; set; }
 
         public string AssetId { get; set; }
 
-        public string SoldBy { get; set; }
+        public HistoryOperationType Type { get; set; }
+
+        public string EmployeeEmail { get; set; }
+
+        public string TxHash { get; set; }
 
         public int BlockHeight { get; set; }
 
         public int BlockConfirmations { get; set; }
-
-        public string TxHash { get; set; }
     }
 }
