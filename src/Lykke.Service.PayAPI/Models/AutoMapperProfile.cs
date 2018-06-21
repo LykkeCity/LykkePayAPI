292183c0f44f31988c5c754f74057df426567142
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-06-21 14:42:12 +0300

    LPDEV-547 LPDEV-548 LPDEV-604 add api to mark, cancel invoice as dispute  and to get list of invoices marked as dispute

--- src/Lykke.Service.PayAPI/Models/AutoMapperProfile.cs
+++ src/Lykke.Service.PayAPI/Models/AutoMapperProfile.cs
@@ -3,6 +3,7 @@ using Common;
 using Lykke.Service.PayAPI.Core.Domain.MerchantWallets;
 using Lykke.Service.PayAPI.Core.Domain.PaymentRequest;
 using Lykke.Service.PayAPI.Core.Domain.Rates;
+using Lykke.Service.PayAPI.Models.Invoice;
 using Lykke.Service.PayCallback.Client.Models;
 using Lykke.Service.PayInternal.Client.Models.Asset;
 using Lykke.Service.PayInternal.Client.Models.PaymentRequest;
@@ -53,6 +54,10 @@ namespace Lykke.Service.PayAPI.Models
                 .ForMember(dest => dest.SettlementMonthPeriod, opt => opt.Ignore())
                 .ForMember(dest => dest.LogoUrl, opt => opt.Ignore())
                 .ForMember(dest => dest.MerchantName, opt => opt.Ignore());
+
+            CreateMap<InvoiceResponseModel, InvoiceMarkedDisputeResponse>(MemberList.Destination)
+                .ForMember(dest => dest.DisputeRaisedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.DisputeReason, opt => opt.Ignore());
 
             CreateMap<MerchantWalletBalanceLine, MerchantWalletConvertedBalanceResponse>(MemberList.Destination)
                 .ForMember(dest => dest.WalletId, opt => opt.MapFrom(src => src.MerchantWalletId));
---
+++ src/Lykke.Service.PayAPI/Models/Invoice/CancelInvoiceDisputeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Lykke.Service.PayAPI.Models.Invoice
+{
+    public class CancelInvoiceDisputeRequest
+    {
+        [Required]
+        public string InvoiceId { get; set; }
+    }
+}
