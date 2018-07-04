be5172486e819435ff70b8901b4b481ce28789ca
tarurar <tarutin_andrew@mail.ru>
2018-07-04 13:06:22 +0300

    added endpoint to get list of available bank cash out assets

---
+++ src/Lykke.Service.PayAPI.Core/Services/IAssetSettingsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Lykke.Service.PayAPI.Core.Services
+{
+    public interface IAssetSettingsService
+    {
+        IReadOnlyList<string> GetCashoutAssets();
+    }
+}
