21db7e15d41385b16aeea04185fdab4c9154edcf
tarurar <tarutin_andrew@mail.ru>
2018-07-04 13:47:56 +0300

    added cashout controller stub

---
+++ src/Lykke.Service.PayAPI/Models/Mobile/Cashout/CashoutModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Lykke.Service.PayAPI.Validation;
+
+namespace Lykke.Service.PayAPI.Models.Mobile.Cashout
+{
+    /// <summary>
+    /// Cashout operation details
+    /// </summary>
+    public class CashoutModel
+    {
+        /// <summary>
+        /// Gets or sets asset id
+        /// </summary>
+        [Required]
+        public string AssetId { get; set; }
+
+        /// <summary>
+        /// Gets or sets amount
+        /// </summary>
+        [Range(Double.Epsilon, Double.MaxValue, ErrorMessage = "Cashout amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets desired cashout asset
+        /// </summary>
+        [CashoutAssetExists]
+        public string DesiredCashoutAsset { get; set; }
+    }
+}
