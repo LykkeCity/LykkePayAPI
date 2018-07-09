f503d86191a1874458dacd460d9b3d9c50a604d9
tarurar <tarutin_andrew@mail.ru>
2018-07-09 22:07:43 +0300

    fixed error message for email validation

--- src/Lykke.Service.PayAPI/Validation/EmailAddressAttribute.cs
+++ src/Lykke.Service.PayAPI/Validation/EmailAddressAttribute.cs
@@ -7,9 +7,16 @@ namespace Lykke.Service.PayAPI.Validation
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
     public class EmailAddressAttribute : ValidationAttribute
     {
+        private const string Message = "The {0} field is not a valid e-mail address.";
+
         public override bool IsValid(object value)
         {
             return value?.ToString().IsValidEmail() ?? false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(Message, name);
         }
     }
 }
