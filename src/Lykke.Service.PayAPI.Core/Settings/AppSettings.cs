77219bf0445be7ed5a86a2e03b93c73cbb634e9e
tarurar <tarutin_andrew@mail.ru>
2018-08-22 21:49:15 +0300

    Merge branch 'feature_LPDEV-793-migrate-to-paymerchant' into dev
    # Conflicts:
    #	src/Lykke.Service.PayAPI.Core/Lykke.Service.PayAPI.Core.csproj

--- src/Lykke.Service.PayAPI.Core/Settings/AppSettings.cs
+++ src/Lykke.Service.PayAPI.Core/Settings/AppSettings.cs
@@ -46,7 +46,7 @@ namespace Lykke.Service.PayAPI.Core.Settings
 
     public class MonitoringServiceClientSettings
     {
-        [HttpCheck("api/isalive", false)]
+        [HttpCheck("api/isalive")]
         public string MonitoringServiceUrl { get; set; }
     }
 }
